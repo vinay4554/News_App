3a4ea38c8784f21978715e249a4a08e0073e9115
vinay4549 <vinaychittibommala@gmail.com>
2022-04-22 00:43:46 +0530

    changes made

--- src/components/Everyheadline.js
+++ src/components/Everyheadline.js
@@ -4,24 +4,31 @@ import { useSelector } from "react-redux";
 import { Everynewsaction } from "../actions/Newsaction";
 import Everyheadlinecard from "./Everyheadlinecard";
 import Allnewssearch from "./Allnewssearch";
-import { v4 as uuidv4 } from 'uuid';
-function Everyheadline(){
-     const dispatch=useDispatch()
-     useEffect(() => {
-     dispatch(Everynewsaction())
-     },[])
-     const everyheadline=useSelector((state) => state["everynews"].dailynews)
-    return(
-        <>
-        <Allnewssearch/>
-            <div className="cards">
-        {everyheadline.length !==0 && 
-
-        everyheadline.map(article => 
-            <Everyheadlinecard article={article} key={uuidv4()} />)
-        } 
-        </div>
-        </>
-    )
+import { v4 as uuidv4 } from "uuid";
+function Everyheadline() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(Everynewsaction());
+  }, []);
+  const everyheadline = useSelector((state) => state["everynews"].dailynews);
+  return (
+    <>
+      <Allnewssearch />
+      <div className="cards">
+        {everyheadline.length > 0 ? (
+          everyheadline.map((article) => (
+            <Everyheadlinecard article={article} key={uuidv4()} />
+          ))
+        ) : (
+          <div class="loading-container">
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 export default Everyheadline;
--- src/components/Topheadlines.js
+++ src/components/Topheadlines.js
@@ -2,20 +2,27 @@ import react from "react";
 import { useSelector } from "react-redux";
 import Searchbar from "./Searchbar";
 import Topheadlinescard from "./Topheadlinescard";
-import { v4 as uuidv4 } from 'uuid';
-function Topheadlines(){
-    const news=useSelector((state) => state["news"].topheadlines)
-    return(
-        <>
-         <Searchbar />
-        <div className="cards">
-        {news.length !==0 && 
-
-        news.map(article => 
-            <Topheadlinescard article={article} key={uuidv4()}/>)
-        } 
-        </div>
-        </>
-    )
+import { v4 as uuidv4 } from "uuid";
+function Topheadlines() {
+  const news = useSelector((state) => state["news"].topheadlines);
+  return (
+    <>
+      <Searchbar />
+      <div className="cards">
+        {news.length > 0 ? (
+          news.map((article) => (
+            <Topheadlinescard article={article} key={uuidv4()} />
+          ))
+        ) : (
+          <div class="loading-container">
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 export default Topheadlines;
